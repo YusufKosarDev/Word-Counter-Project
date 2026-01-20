dc2c04387c03a3407a302cdc3024386debd4922c
YusufKosarDev <yusufkosare004@gmail.com>
2026-01-20 16:56:35 +0300

    Add Word Counter Project

---
+++ index.js
@@ -0,0 +1,22 @@
+let inputText = document.querySelector(".input-text");
+let wordCount = document.querySelector(".word-count");
+let characterCount = document.querySelector(".character-count");
+let sentenceCount = document.querySelector(".sentence-count");
+let paragraphCount = document.querySelector(".paragraph-count");
+
+inputText.addEventListener("input", () => {
+
+    characterCount.textContent = inputText.value.length;
+
+
+
+
+
+    let textTrim = inputText.value.trim();
+
+    wordCount.textContent = textTrim.split(/\s+/).filter((item) => item).length;
+
+    sentenceCount.textContent = textTrim.split(".").filter((item) => item).length;
+
+    paragraphCount.textContent = textTrim.split("\n\n").filter((item) => item).length;
+});
